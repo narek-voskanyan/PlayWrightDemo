735395fba3b00f5f210ab7402aaeb92aec8e38fa
narek-voskanyan <narek.voskanyan91@gmail.com>
2024-04-15 15:15:26 +0400

    Adding POM for All Itemspage

--- page-objects/AllItemsPOM.js
+++ page-objects/AllItemsPOM.js
@@ -1,1 +1,19 @@
 const { expect } = require('@playwright/test');
+
+exports.AllItemsPage = class AllItemsPage{
+    constructor(page){
+        this.page = page;
+        this.shoppingCartButton = page.locator('#shopping_cart_container');
+        this.filterButton = page.locator('.select_container');
+    
+    }
+
+    async clickOnYourCarButton(){
+        await expect(this.shoppingCartButton).toBeVisible();
+        await this.shoppingCartButton.click();
+    }
+    async checkYourCartURL(cartURL){
+        await expect(this.page).toHaveURL(cartURL);
+    }
+    
+}
--- tests/All Items/allItems.spec.js
+++ tests/All Items/allItems.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 const {LoginPage} = require('../../page-objects/LoginPagePOM.js');
+const {LaftMenuBarPage} = require('../../page-objects/LeftMenuBarPOM.js');
+const {AllItemsPage} = require('../../page-objects/AllItemsPOM.js');
 
 const USERNAME = process.env.USER_NAME;
 const PASSWORD = process.env.PASSWORD;
@@ -9,7 +11,7 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+  
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
@@ -25,7 +27,7 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allItemsPage = new AllItemsPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
@@ -39,18 +41,22 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
 
 
   test('Verify that the Your Cart icon exists and, upon clicking, switches to the Your Cart page', async ({ page }) => {
+    const aaa ="hdhfjh"
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allItemsPage = new AllItemsPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
+    //Check that All itames page open
+    await loginPage.checkAllItemsPageURL();
     //Click to "Your cart" icon
-   await page.locator('.shopping_cart_link').click();
+    await allItemsPage.clickOnYourCarButton();
+    
     // Check that redirect to "Your cart" page.
-    await expect(page).toHaveURL('/cart.html');
+    await allItemsPage.checkYourCartURL('/cart.html');
 
   });
 
@@ -58,43 +64,32 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const leftMenuBarPage = new LaftMenuBarPage(page); 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
   
     // Click to "Left menu bur" button for open pop-up
-   await page.locator('#react-burger-menu-btn').click();
+   await leftMenuBarPage.clickOnOpenButton();
   
-    // The "left menu bar" pop-up should be open
-    await expect(page.waitForSelector('.bm-menu-wrap')).toBeTruthy();
-
-    
-    await expect(page.$('.bm-menu-wrap')).toBeTruthy();
   });
 
 
     //STUGEL TO BE VISIBLE
 
-  test('Verify that the "Filter" button exists and, upon clicking, a pop-up should open', async ({ page }) => {
+  test('Verify that the "Filter" button exists', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allItemsPage = new AllItemsPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
   
-    // Click to "Filter" button for open pop-up
-   await page.locator('.select_container').click();
-  
-    // The "Filter" pop-up should be open
-    await expect(page.waitForSelector('.product_sort_container')).toBeTruthy();
-
-    await expect(page.$('.product_sort_container')).toBeTruthy();
-
+    // Check that Filter buton exist
+   await expect(allItemsPage.filterButton).toBeVisible();
   });
 
 
@@ -121,6 +116,8 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
+    const allItemsPage = new AllItemsPage(page);
+    const leftMenuBarPage = new LaftMenuBarPage(page); 
 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
@@ -131,15 +128,16 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     
     //Click to "Your cart" icon
-    await page.locator('.shopping_cart_link').click();
+    await allItemsPage.clickOnYourCarButton();
+    
     // Check that chosen product exist
     await expect(page.waitForSelector('.bm-menu-wrap')).toBeTruthy();
 
     // Click to "Left menu bur" button for open pop-up
-    await page.locator('#react-burger-menu-btn').click();
+    await leftMenuBarPage.clickOnOpenButton();
 
     //Click on "Logout" for finish corrent session
-    await page.locator('#logout_sidebar_link').click();
+    await leftMenuBarPage.logout.click();
 
     // Perform authentication steps. Sign in valid user account csecond time.
 
@@ -150,11 +148,9 @@ test('Verify that "Swag Labs" title exist on page', async ({ page }) => {
 
 
     //Click to "Your cart" icon
-    await page.locator('.shopping_cart_link').click();
+    await allItemsPage.clickOnYourCarButton();
     // Check that chosen product exist
-    await expect(page.waitForSelector('.cart_item_label')).toBeTruthy();
-
-
+    await expect(page.getByText('Sauce Labs Backpackcarry.')).toBeVisible();
   });
 
 
