c9e32b057603b348978c3b160e3649dad8635bb7
narek-voskanyan <narek.voskanyan91@gmail.com>
2024-04-16 15:47:11 +0400

    Add YourCartPOM.js locators and functions.Bbuild up test case in yourCart.spec.js

--- page-objects/YourCartPOM.js
+++ page-objects/YourCartPOM.js
@@ -2,6 +2,35 @@ const { expect } = require('@playwright/test');
 
 exports.YourCartPage = class YourCartPage{
     constructor(page){
-        
+        this.page = page;
+        this.continueShoppingButton = page.locator('#continue-shopping');
+        this.checkoutButton = page.locator('[data-test="checkout"]');
+        this.title = page.locator('[data-test="title"]');
+        this.cartQuantityLable = page.locator('[data-test="cart-quantity-label"]');
+        this.description = page.locator('[data-test="cart-desc-label"]');
+    }
+    //Click on Continue Shoppin button
+    async clickOnContinueShopping(){
+        await this.continueShoppingButton.click();
+    }
+    //Click on Checkout button
+    async clickOnCheckoutButton(){
+        await this.checkoutButton.click();
+    }
+    //Check All Items page URL
+    async checkURL(URL){
+        await expect(this.page).toHaveURL(URL);
+    }
+    //Check Titel field value
+    async checkTitleValue(titleText){
+        await expect(this.title).toHaveText(titleText);
+    }
+    //Check QTY field value
+    async checkQuantityLableValue(QTY){
+        await expect(this.cartQuantityLable).toHaveText(QTY);
+    }
+    //Check Description field value
+    async checkDescriptionValue(description){
+        await expect(this.description).toHaveText(description);
     }
 }
--- tests/Your Cart/yourCart.spec.js
+++ tests/Your Cart/yourCart.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 const {LoginPage} = require('../../page-objects/LoginPagePOM.js');
+const {YourCartPage} = require('../../page-objects/YourCartPOM.js');
+const {AllItemsPage} = require('../../page-objects/AllItemsPOM.js');
 
 
 const USERNAME = process.env.USER_NAME;
@@ -9,29 +11,33 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allitems = new AllItemsPage(page);
+    const yourCartPage = new YourCartPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
     
     //Click on the "Your cart" field
-    await page.locator('//a[@class="shopping_cart_link"]').click();
+    await allitems.clickOnYourCarButton();
+
     //Check that the "Your Cart" page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
    
     //Check that the "Your Cart" title exist
-    await expect(page.locator('//span[@class="title"]')).toHaveText('Your Cart');
+    await yourCartPage.checkTitleValue('Your Cart');
+
     //Check that the "QTY" field exist
-    await expect(page.locator('//div[@class="cart_quantity_label"]')).toHaveText('QTY');
+    await yourCartPage.checkQuantityLableValue('QTY');
+
     //Check that the "Description" field exist
-    await expect(page.locator('//div[@class="cart_desc_label"]')).toHaveText('Description');
+    await yourCartPage.checkDescriptionValue('Description');
 
     //Check that the "Continue Shopping" button exist
-    await expect(page.locator('#continue-shopping').getByText('Continue Shopping')).toBeVisible();
+    await expect(yourCartPage.continueShoppingButton).toBeVisible();
 
     //Check that the "Checkout" button exist
-    await expect(page.locator('//button[@data-test="checkout"]').getByText('Checkout')).toBeVisible();
+    await expect(yourCartPage.checkoutButton).toBeVisible();
     
   });
 
@@ -39,6 +45,8 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
+    const allitems = new AllItemsPage(page);
+    const yourCartPage = new YourCartPage(page);
 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
@@ -46,38 +54,42 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     await loginPage.clickLoginButton();
     
     //Click on the "Your cart" field
-    await page.locator('//a[@class="shopping_cart_link"]').click();
+    await allitems.clickOnYourCarButton();
+    
     //Check that the "Your Cart" page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
    
-
     //Click on the "Continue Shopping" button
-    await page.locator('#continue-shopping').click();
-
-    await expect(page).toHaveURL('/inventory.html');
+    await yourCartPage.clickOnContinueShopping();
+    //Check that all items page open
+    await yourCartPage.checkURL('/inventory.html');
   });
 
   test('Verify that upon clicking the "Checkout" button, the "Checkout: Your Information" page should be displayed', async ({ page }) => {
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
+    const allitems = new AllItemsPage(page);
+    const yourCartPage = new YourCartPage(page);
 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
     
-    //Click on the "Your cart" field
-    await page.locator('//a[@class="shopping_cart_link"]').click();
+      //Click on the "Your cart" field
+     await allitems.clickOnYourCarButton();
     //Check that the "Your Cart" page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
+
    //Check the color of "Checout" field
-   await expect(page.locator('//button[@data-test="checkout"]')).toHaveCSS('background-color','rgb(61, 220, 145)');
+   await expect(yourCartPage.checkoutButton).toHaveCSS('background-color','rgb(61, 220, 145)');
 
     //Click on the "Checout" button
-    await page.locator('//button[@data-test="checkout"]').click();
+    await yourCartPage.clickOnCheckoutButton();
+
     //Check that "Checout your information" page display
-    await expect(page).toHaveURL('/checkout-step-one.html');
+    await yourCartPage.checkURL('/checkout-step-one.html');
   });
 
 //work correct only if there is one product
@@ -85,6 +97,7 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
+    const allitems = new AllItemsPage(page);
 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
@@ -95,19 +108,17 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
      await page.locator('#add-to-cart-sauce-labs-backpack').click();
 
     //Click on the "Your cart" field
-    await page.locator('//a[@class="shopping_cart_link"]').click();
+    await allitems.clickOnYourCarButton();
     //Check that the "Your Cart" page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
 
     //Check that added product display on the screen
     await expect(page.locator('//div[@class="cart_list"]/div[3]')).toBeVisible();
     
-   
     //Click on the 'Remove' button in the product field
     await page.locator('#remove-sauce-labs-backpack').click();
 
     //Check that added product display on the screen
-   
     await expect(page.locator('//div[@class="cart_list"]/div[3]')).not.toBeVisible();
     await expect(page.locator('.cart_list > :nth-child(3)')).toHaveClass('removed_cart_item');
    
@@ -118,6 +129,8 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
+    const allitems = new AllItemsPage(page);
+    const yourCartPage = new YourCartPage(page);
 
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
@@ -126,18 +139,16 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
 
      
     //Click on the "Your cart" field
-    await page.locator('//a[@class="shopping_cart_link"]').click();
+    await allitems.clickOnYourCarButton();
     //Check that the "Your Cart" page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
 
     //Check that there is no any added product
     await expect(page.locator('//div[@class="cart_item"][1]')).not.toBeVisible();
     
-    //Click on the "Checkout" button
-    await page.locator("//button[@id='checkout']").click();
 
     //Veriy that button is not clicable
-    await expect(page.locator("//button[@id='checkout']")).toBeDisabled();
+    await expect(yourCartPage.checkoutButton).toBeDisabled();
    
   });
 
@@ -145,14 +156,14 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allitems = new AllItemsPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
 
      // Check that corrent page open
-     await expect(page).toHaveURL('/inventory.html');
+     await loginPage.checkAllItemsPageURL();
 
      //Take title, description and price value from product
      const title = await page.locator("#item_4_title_link").innerText();
@@ -163,10 +174,10 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
      await page.locator('#add-to-cart-sauce-labs-backpack').click();
 
     //Click on the "Your cart" button 
-    await page.locator('#shopping_cart_container').click();
+    await allitems.clickOnYourCarButton();
 
     //Check that coresspond page open
-    await expect(page).toHaveURL('/cart.html');
+    await allitems.checkYourCartURL('/cart.html');
 
     //Check that the added  product fields values same with chosen product
     await expect(page.locator('#item_4_title_link')).toHaveText(title);
@@ -178,20 +189,17 @@ test('Verify that the "Your Cart" page contains QTY, Description tittels, "Conti
     
     // Perform authentication steps. Sign in valid user account.
     const loginPage = new LoginPage(page);
-
+    const allitems = new AllItemsPage(page);
     await loginPage.goto();
     await loginPage.fillUsernameField(USERNAME);
     await loginPage.fillPasswordfield(PASSWORD);
     await loginPage.clickLoginButton();
 
      // Check that corrent page open
-     await expect(page).toHaveURL('/inventory.html');
-    
+    await loginPage.checkAllItemsPageURL();
      //Click on the "Add to cart" button in same product for add it to the "Your cart"
      await page.locator('#add-to-cart-sauce-labs-backpack').click();
 
     //Check that the product number apear in the red circle on the "Your cart" button
-    await expect(page.locator('//span[@data-test="shopping-cart-badge"]')).toHaveText('1');
-
-  
+    await expect(allitems.shoppingCartPageRedCircle).toHaveText('1');
   });
